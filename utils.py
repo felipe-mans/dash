5349ec5f662a54814b0ba11ecf72b1ee1e9ff3e2
Felipe Mansilla <pipe.ny@gmail.com>
2016-01-04 22:40:49 -0500

    weather api working for current temperature

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 import urllib2, json
 
-#api key: bac9dac8506a2912
-def getWeather():
-  f = urllib2.urlopen('http://api.wunderground.com/api/bac9dac8506a2912/geolookup/conditions/q/IA/Cedar_Rapids.json')
+# api key: bac9dac8506a2912
+# Needs State abbreviation
+# Needs City name
+def getWeather(StateAbbrv, CityName):
+  f = urllib2.urlopen('http://api.wunderground.com/api/bac9dac8506a2912/geolookup/conditions/q/NY/Forest_Hills.json')
   json_string = f.read()
   parsed_json = json.loads(json_string)
   location = parsed_json['location']['city']
   temp_f = parsed_json['current_observation']['temp_f']
-  print "Current temperature in %s is: %s" % (location, temp_f)
+  result = "Current temperature in %s is: %s" % (location, temp_f)
   f.close()
+  return result
