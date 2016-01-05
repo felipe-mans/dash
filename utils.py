ad5d1e82ca21f9c73105718f2dca321aae7f7e65
Felipe Mansilla <pipe.ny@gmail.com>
2016-01-04 22:32:21 -0500

    added key

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import urllib2, json
 
 #api key: bac9dac8506a2912
 def getWeather(CityName):
-  f = urllib2.urlopen('http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json')
+  f = urllib2.urlopen('http://api.wunderground.com/api/bac9dac8506a2912/geolookup/conditions/q/IA/Cedar_Rapids.json')
   json_string = f.read()
   parsed_json = json.loads(json_string)
   location = parsed_json['location']['city']
