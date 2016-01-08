bb381264fb58e6d7c992fe9c858c45a1a123aff4
felipe-mans <pipe.ny@gmail.com>
2016-01-08 06:45:37 -0500

    useful comments added

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 import urllib2, json
 
+#----------------------- API Functions ----------------------------------
 
-#Parameters: Abbreviation of the state, the name of the city
+#---------- getWeatherByCity ----------
+#
+# Will return the current weather of the given city
+#
+# Parameters: Abbreviation of the state, the name of the city
 def getWeatherByCity(StateAbbrv, CityName):
   key = ""
 
@@ -13,12 +18,34 @@ def getWeatherByCity(StateAbbrv, CityName):
   location = parsed_json['location']['city']
   temp_f = parsed_json['current_observation']['temp_f']
   weather = parsed_json['current_observation']['weather']
-  
   result = "%s: %s, %s degrees F." % (location, weather, temp_f)
   f.close()
   return result
 
-#Parameters: Section of the New York Times (home, sports, etc.)
+#---------- getWeatherByZip ---------
+#
+# Will return the weather of the Zipcode  given
+#
+# Parameters: Zipcode of the area you want (in the U.S.)
+def getWeatherByZip(zip):
+  key = ""
+  f = urllib2.urlopen('http://api.wunderground.com/api/%s/geolookup/conditions/q/%s.json' % (key, zip))
+  json_string = f.read()
+  parsed_json = json.loads(json_string)
+  location = parsed_json['location']['city']
+  temp_f = parsed_json['current_observation']['temp_f']
+  weather = parsed_json['current_observation']['weather']
+  result = ('%s (%s): %s. %s degrees F.' % (location, zip, weather, temp_f))
+  f.close()
+  return result
+                      
+
+
+#---------- getTopStories ----------
+#
+# Will return the top 5 stories of the given section from the New York Time
+#
+# Parameters: Section of the New York Times (home, sports, etc.)
 def getTopStories(section):
   key = ''
   f = urllib2.urlopen('http://api.nytimes.com/svc/topstories/v1/%s.json?api-key=%s' % (section, key))
@@ -30,29 +57,15 @@ def getTopStories(section):
     result = result + parsed_json['results'][i]['title'] + '\n'
     i = i+1
   return result
-    
-#print getWeatherByCity('CA', 'San_Francisco') 
-#print getWeatherByCity('NY', 'Brooklyn')
-#print getWeatherByCity('CA', 'Los_Angeles')
-print getTopStories('home')
 
-#Parameters: Zipcode of the area you want (in the U.S.)
-def getWeatherByZip(zip):
-  key = ""
 
-  f = urllib2.urlopen('http://api.wunderground.com/api/%s/geolookup/conditions/q/%s.json' % (key, zip))
-  #http://api.wunderground.com/api/Your_Key/forecast/q/CA/San_Francisco.json
-  json_string = f.read()
-  parsed_json = json.loads(json_string)
-	
-  location = parsed_json['location']['city']
-  temp_f = parsed_json['current_observation']['temp_f']
-  weather = parsed_json['current_observation']['weather']
-  
-  result = ('%s (%s): %s. %s degrees F.' % (location, zip, weather, temp_f))
-  f.close()
-  return result
+
+#----------------------- Testing Statements --------------------------------
 
 #print getWeatherByZip(10024)
 #print getWeatherByZip(90210)
 #print getWeatherByZip(94101)
+#print getWeatherByCity('CA', 'San_Francisco')
+#print getWeatherByCity('NY', 'Brooklyn')
+#print getWeatherByCity('CA', 'Los_Angeles')
+print getTopStories('home')
