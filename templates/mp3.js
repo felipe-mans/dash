76817305bc91c7f8b39e9aa5d19df9d0fecd884a
daltonwu <dalton.z.wu@gmail.com>
2016-01-11 12:05:34 -0500

    Wow, I was being stupid

--- templates/mp3.js
+++ templates/mp3.js
@@ -1,21 +1,7 @@
 document.getElementById("audioInput").addEventListener("change", function() {
-	console.log("Something changed!");
-	
     var audio = document.getElementById("audio");
-    var audioInput = document.getElementById("audioInput")
-	var filename = audioInput.value.split('\\').pop();
-	
-	console.log("Audio filename = " + filename);
-	
-	audio.setAttribute("src", filename);
-	
-	if(filename.slice(-3) === "mp3") {
-		audio.setAttribute("type", "audio/mpeg");
-	}
-	else if(filename.slice(-3) === "ogg") {
-	    audio.setAttribute("type", "audio/ogg");
-	}
-	else if(filename.slice(-3) === "wav") {
-	    audio.setAttribute("type", "audio/wav");
-	}
+    var audioInput = document.getElementById("audioInput");
+    var file = this.files[0];
+    var fileURL = URL.createObjectURL(file);
+    audio.src = fileURL;
 });
