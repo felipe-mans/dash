51603d89c31914b015f57e8d52466691e1ddd50b
Dalton Wu <dalton.z.wu@gmail.com>
2016-01-24 14:21:50 -0500

    Updated digital

--- static/digitalClock.js
+++ static/digitalClock.js
@@ -1,16 +1,26 @@
-displayClock();
-setInterval(displayClock, 1000);
+var twelveHours = true;
+displayClock(twelveHours);
+setInterval(displayClock, 1000, twelveHours);
 
-function displayClock() {
+function displayClock(twelveHours) {
     var clock = document.getElementById("digitalClock");
     var now = new Date();
     var hour = now.getHours();
     var minute = now.getMinutes();
     var second = now.getSeconds();
     
-    // ~~ is equivalent to Math.floor here
-    var time = ["" + ~~(hour/10) + hour % 10,
+	var hh;
+	var am_pm = "";
+	if(twelveHours) {
+	    // ~~ is equivalent to Math.floor here
+		hh = hour % 12;
+		am_pm = hour >= 12 ? " PM" : " AM";
+	}
+	else {
+		hh = "" + ~~(hour/10) + hour % 10;
+	}
+   	var time = [hh,
                 "" + ~~(minute/10) + minute % 10,
                 "" + ~~(second/10) + second % 10];
-    clock.innerHTML = time.join(":") + (hour >= 12 ? " p.m." : " a.m.");
+    clock.innerHTML = time.join(":") + am_pm;
 };
