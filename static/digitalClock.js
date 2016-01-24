f84832f23ced33744a563b33ea12760bf1a7ae2f
Dalton Wu <dalton.z.wu@gmail.com>
2016-01-24 14:46:35 -0500

    Digital clock date added

--- static/digitalClock.js
+++ static/digitalClock.js
@@ -1,4 +1,28 @@
 var twelveHours = true;
+
+var days = new Array(7);
+days[0] = "Sunday";
+days[1] = "Monday";
+days[2] = "Tuesday";
+days[3] = "Wednesday";
+days[4] = "Thursday";
+days[5] = "Friday";
+days[6] = "Saturday";
+
+var months = new Array(12);
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[12] = "December";
+
 displayClock(twelveHours);
 setInterval(displayClock, 1000, twelveHours);
 
@@ -8,9 +32,9 @@ function displayClock(twelveHours) {
     var hour = now.getHours();
     var minute = now.getMinutes();
     var second = now.getSeconds();
-    
 	var hh;
-	var am_pm = "";
+	var am_pm, date = "";
+	
 	if(twelveHours) {
 	    // ~~ is equivalent to Math.floor here
 		hh = hour % 12;
@@ -19,8 +43,12 @@ function displayClock(twelveHours) {
 	else {
 		hh = "" + ~~(hour/10) + hour % 10;
 	}
+	
    	var time = [hh,
                 "" + ~~(minute/10) + minute % 10,
                 "" + ~~(second/10) + second % 10];
-    clock.innerHTML = time.join(":") + am_pm;
+	
+	
+    clock.innerHTML = time.join(":") + am_pm + "<br>"
+	                  + days[now.getDay()] + ", " + months[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
 };
